a3bad47d244e8976df83cc7de891b07ec9bd0ca1
rober3847 <34666047+rober3847@users.noreply.github.com>
2025-10-15 11:53:28 +0100

    Update uart_component_esp32_arduino.cpp

--- components/uart/uart_component_esp32_arduino.cpp
+++ components/uart/uart_component_esp32_arduino.cpp
@@ -189,7 +189,10 @@ void ESP32ArduinoUARTComponent::check_logger_conflict() {
   }
 
   // if (this->hw_serial_ == logger::global_logger->get_hw_serial()) {
-  if (logger::global_logger != nullptr && logger::global_logger->get_uart() == this) {
+  // if (logger::global_logger != nullptr && logger::global_logger->get_uart() == this) {
+  if (logger::global_logger != nullptr &&
+    logger::global_logger->get_uart() == logger::UART_SELECTION_UART0 &&
+    this->hw_serial_ == &Serial) {
     ESP_LOGW(TAG, "  You're using the same serial port for logging and the UART component. Please "
                   "disable logging over the serial port by setting logger->baud_rate to 0.");
   }
